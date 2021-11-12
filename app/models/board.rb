57bba2e60b564363adac07219aac6ce76e2341b4
lgunds6 <lgunds6@gmail.com>
2021-11-12 14:48:10 +1100

    Updated views in controller and added form for new lisitng

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,6 @@
 class Board < ApplicationRecord
+    validates :title, presence: true
+    validates :condition, presence: true
+    validates :description, presence: true, length: {minimum: 10}
+    validates :price, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  
+resources :store
+
   root 'store#index'
 
-  get '/store', to: 'store#index'
-  get '/store/:id', to: 'store#show'
-  get '/store/new', to: 'store#new'
-  post '/store', to: 'store#create'
-  get '/store/:id/edit', to: 'store#edit'
-  patch '/store/:id', to: 'store#update'
-  delete '/store/:id', to: 'store#destroy'
+  # get '/store', to: 'store#index'
+  # post '/store/create', to: 'store#create'
+  # get '/store/new', to: 'store#new'
+  # get '/store/:id', to: 'store#show'
+  # get '/store/:id/edit', to: 'store#edit'
+  # patch '/store/:id', to: 'store#update'
+  # delete '/store/:id', to: 'store#destroy'
 end
