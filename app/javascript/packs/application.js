52dc45da15e0d57c406bc36730ddc6d473e82529
lgunds6 <lgunds6@gmail.com>
2021-11-17 17:26:40 +1100

    added swifttail.css to test

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,13 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// app/javascript/packs/application.js
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+// Tailwind CSS
+import "stylesheets/application" // ADD THIS LINE
